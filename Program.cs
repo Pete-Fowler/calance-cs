7ddfcd936031445cbdbc87aa580a512d7aec77b9
Pete Fowler <fowler.pete@gmail.com>
2022-12-02 19:58:42 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Add("User-Agent", "Bootstrap release to csv script");
+
+await ProcessReleasesAsync(client);
+
+static async Task ProcessReleasesAsync(HttpClient client)
+{
+     var json = await client.GetStringAsync(
+         "https://api.github.com/repos/twbs/bootstrap/releases");
+
+     Console.Write(json);
+}
