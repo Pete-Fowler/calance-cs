92d709aca38bc2308e465fd75a0681c2d82dad99
Pete Fowler <fowler.pete@gmail.com>
2022-12-02 20:14:18 -0700

    Get and print tag_name

--- Program.cs
+++ Program.cs
@@ -12,9 +12,12 @@ await ProcessReleasesAsync(client);
 static async Task ProcessReleasesAsync(HttpClient client)
 {
 await using Stream stream =
-    await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-var repositories =
+    await client.GetStreamAsync("https://api.github.com/repos/twbs/bootstrap/releases");
+var releases =
     await JsonSerializer.DeserializeAsync<List<Release>>(stream);
+
+    foreach (var release in releases ?? Enumerable.Empty<Release>())
+      Console.Write(release.tag_name);
 }
 
 //         "https://api.github.com/repos/twbs/bootstrap/releases");
