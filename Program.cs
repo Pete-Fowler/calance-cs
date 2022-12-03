88b4e82c90abe594d275140bc6a87eec4bde0755
Pete Fowler <fowler.pete@gmail.com>
2022-12-03 07:12:51 -0700

    Format code

--- Program.cs
+++ Program.cs
@@ -9,20 +9,19 @@ client.DefaultRequestHeaders.Add("User-Agent", "Bootstrap release to csv script"
 
 var releases = await ProcessReleasesAsync(client);
 
-foreach (var release in releases ?? Enumerable.Empty<Release>()) {
+foreach (var release in releases ?? Enumerable.Empty<Release>())
+{
   Console.WriteLine(release.created_at);
   Console.WriteLine(release.tag_name);
   Console.WriteLine(release.assets?[0].browser_download_url);
 }
 
-
 static async Task<List<Release>> ProcessReleasesAsync(HttpClient client)
 {
-await using Stream stream =
-    await client.GetStreamAsync("https://api.github.com/repos/twbs/bootstrap/releases");
+  await using Stream stream =
+      await client.GetStreamAsync("https://api.github.com/repos/twbs/bootstrap/releases");
 
-var releases =
-    await JsonSerializer.DeserializeAsync<List<Release>>(stream);
-    return releases ?? new();
-   
+  var releases =
+      await JsonSerializer.DeserializeAsync<List<Release>>(stream);
+  return releases ?? new();
 }
--- Release.cs
+++ Release.cs
@@ -1,17 +1,17 @@
 using System.Text.Json.Serialization;
-  public class Asset
-    {
-        public string? browser_download_url { get; set; }
-    }
+public class Asset
+{
+  public string? browser_download_url { get; set; }
+}
 
-  public class Author
-  {
-      public string? login { get; set; }
-  }
+public class Author
+{
+  public string? login { get; set; }
+}
 
-  public class Release
-  {
-      public List<Asset>? assets { get; set; }
-      public string? tag_name { get; set; }
-      public DateTime created_at { get; set; }
-  }
+public class Release
+{
+  public List<Asset>? assets { get; set; }
+  public string? tag_name { get; set; }
+  public DateTime created_at { get; set; }
+}
