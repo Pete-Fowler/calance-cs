8d8a197f2fc7c9005a5828b7caddfefd74ae60de
Pete Fowler <fowler.pete@gmail.com>
2022-12-03 07:58:58 -0700

    Edit .gitignore

--- Program.cs
+++ Program.cs
@@ -3,28 +3,31 @@ using System.Text.Json;
 
 using HttpClient client = new();
 client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(
-    new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 client.DefaultRequestHeaders.Add("User-Agent", "Bootstrap release to csv script");
 
 var releases = await ProcessReleasesAsync(client);
 
 List<string?> data = new List<string?>();
+data.Add("Date, Name, Download Url");
 
 foreach (var release in releases ?? Enumerable.Empty<Release>())
 {
-  data.Add(release.created_at?.Substring(0, 10));
-  data.Add(release.tag_name);
-  data.Add(release.assets?[0].browser_download_url);
+    data.Add(
+        $"{release.created_at?.Substring(0, 10)}, {release.tag_name}, {release.assets?[0].browser_download_url}"
+    );
 }
+
+// const csv = data.map((row) => row.join(",")).join("\n");
+
 data.ForEach(i => Console.WriteLine(i));
 
 static async Task<List<Release>> ProcessReleasesAsync(HttpClient client)
 {
-  await using Stream stream =
-      await client.GetStreamAsync("https://api.github.com/repos/twbs/bootstrap/releases");
+    await using Stream stream = await client.GetStreamAsync(
+        "https://api.github.com/repos/twbs/bootstrap/releases"
+    );
 
-  var releases =
-      await JsonSerializer.DeserializeAsync<List<Release>>(stream);
-  return releases ?? new();
+    var releases = await JsonSerializer.DeserializeAsync<List<Release>>(stream);
+    return releases ?? new();
 }
--- Release.cs
+++ Release.cs
@@ -1,17 +1,18 @@
 using System.Text.Json.Serialization;
+
 public class Asset
 {
-  public string? browser_download_url { get; set; }
+    public string? browser_download_url { get; set; }
 }
 
 public class Author
 {
-  public string? login { get; set; }
+    public string? login { get; set; }
 }
 
 public class Release
 {
-  public List<Asset>? assets { get; set; }
-  public string? tag_name { get; set; }
-  public string? created_at { get; set; }
+    public List<Asset>? assets { get; set; }
+    public string? tag_name { get; set; }
+    public string? created_at { get; set; }
 }
